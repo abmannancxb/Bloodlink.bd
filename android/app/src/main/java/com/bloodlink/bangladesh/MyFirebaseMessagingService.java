ac6b9d3546d9fa4b4874ec0023600370462a4c30
abmannancxb <abmannancxb@gmail.com>
2026-07-14 16:26:38 +0400

    feat: import FirebaseMessaging in messaging service

--- android/app/src/main/java/com/bloodlink/bangladesh/MyFirebaseMessagingService.java
+++ android/app/src/main/java/com/bloodlink/bangladesh/MyFirebaseMessagingService.java
@@ -19,6 +19,7 @@ import androidx.core.app.RemoteInput;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FieldValue;
 
